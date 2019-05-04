8603b9d3d679b21597ec3d66ccac9410bd1b98d5
Tracie Masek <traciemasek@gmail.com>
2019-05-04 16:06:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const nestedTarget = () => {
 }
 
 const increaseRankBy = (n) => {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n; 
+  }  
 }
 
 const deepestChild = () => {
